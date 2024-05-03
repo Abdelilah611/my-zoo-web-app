06c3497046a9815ffc0ce4a8901065a96f7378ce
Lorenzo Florenty <lorenzo.florenty@gmail.com>
2024-05-03 10:46:22 -0400

    📦 NEW: Dynamised Habitat page with JS

---
+++ assets/js/habitats-toggle.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('turbo:load', function () {
+    // Select all buttons with an ID
+    const buttons = document.querySelectorAll('button[id]')
+
+    // Loop through each button and add a click event listener
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        // Get the ID of the button
+        const buttonId = button.getAttribute('id')
+
+        // Toggle the corresponding details section
+        const detailsId = `${buttonId}-details`
+        const detailsElement = document.getElementById(detailsId)
+        detailsElement.classList.toggle('hidden')
+      })
+    })
+  })
+})
