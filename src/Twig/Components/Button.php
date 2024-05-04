4bbd8355eb1ba54aaf11cb7d5c4624ee3cb8e6d8
Lorenzo Florenty <lorenzo.florenty@gmail.com>
2024-05-04 10:34:46 -0400

    📦 NEW:  Create the login page style

--- src/Twig/Components/Button.php
+++ src/Twig/Components/Button.php
@@ -11,6 +11,7 @@ class Button
     public string $size;
     public bool $isPrimary;
     public bool $isCta;
+    public bool $isSubmit;
     public string $labelColor;
 
     public function __construct(
@@ -18,12 +19,14 @@ class Button
         string $size = 'medium',
         bool $isPrimary = true,
         bool $isCta = false,
+        bool $isSubmit = false,
         string $labelColor = ''
     ) {
         $this->text = $text;
         $this->size = $size;
         $this->isPrimary = $isPrimary;
         $this->isCta = $isCta;
+        $this->isSubmit = $isSubmit;
         $this->labelColor = $labelColor;
     }
 }
