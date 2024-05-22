610f0e28af70eb76014a67c2d58bef28f63c1e38
Lorenzo Florenty <lorenzo.florenty@gmail.com>
2024-05-21 21:56:07 -0400

    👌 IMPROVE: Lint

--- src/Controller/Post/ContactController.php
+++ src/Controller/Post/ContactController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
@@ -21,14 +22,15 @@ class ContactController extends AbstractController
         $email = (new TemplatedEmail())
             ->from($request->get('email'))
             ->to('lorenzo.florenty@gmail.com')
-            ->subject('You\'ve been contacted by ' . $request->get('name'))
-            ->htmlTemplate('emails/mail.html') 
+            ->subject('You\'ve been contacted by '.$request->get('name'))
+            ->htmlTemplate('emails/mail.html')
             ->context([
-                'name' => $request->get('name'), 
-                'message' => $request->get('message'), 
+                'name' => $request->get('name'),
+                'message' => $request->get('message'),
             ]);
 
         $mailer->send($email);
+
         return new JsonResponse(['message' => 'Email sent successfully!']);
     }
 }
